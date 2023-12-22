e8d5a06a298fcdccb557433232abaf81ed6a089e
मयंक <126972040+MayankDevil@users.noreply.github.com>
2023-12-22 14:50:46 +0000

    Upload all

---
+++ index.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Graduate Government College | PGGC11</title>
+    <link rel="icon" href="img/logo.jpg">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@500&display=swap" rel="stylesheet">
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="overlay" id="overlay"></div>
+    <div class="pop-up" id="pop-up" onload="pop_up()">
+        <button class="close" id="close" title="close">&times;</button>
+        <img src="img/logo.jpg" alt="PGGC11 Logo">
+        <h2>Welcome to Website of Post Graduate Government College Sector 11, Chandigarh</h2>
+        <p>Admission Helpline No. <i class="fa-solid fa-phone"></i> 0172-2740597, 77101-14580</p>
+    </div>
+    <div class="mother-container">
+        <div class="additional">
+            <div class="helpline">
+                <p>Helpline No. <i class="fa-solid fa-phone"></i> 0172-2740597, 77101-14580</p>
+            </div>
+            <div class="zoom-area">
+                <span class="site-map" title="site-map"><i class="fa-solid fa-sitemap"></i></span>
+                <span class="zoom-out" id="zoom-out" title="Zoom-Out">A-</span>
+                <span class="normal-font" id="normal-font" onclick="normal()" title="Normal Font">A</span>
+                <span class="zoom-in" id="zoom-in" onclick="zoom()" title="Zoom-In">A+</span>
+            </div>
+        </div>
+        <div class="header">
+            <div class="logo-area">
+                <div class="logo">
+                    <img src="img/logo.jpg" alt="PGGC11 Logo">
+                </div>
+                <div class="college-name">
+                    <h2>Post Graduate Government College</h2>
+                    <p>Sector 11, Chandigarh</p>
+                </div>
+            </div>
+            <div class="g20">
+                <img src="img/g20.jpg" alt="G20 Logo">
+            </div>
+            <div class="login-area">
+                <a id="staff" href="login/staff-login.php">Staff Login</a>
+                <a id="web" href="login/web-login.php">Web Login</a>
+            </div>
+        </div>
+        
+        <!-- navbar start -->
+        <nav class="navbar navbar-expand-lg bg-body-tertiary bg-light">
+            <div class="container-fluid">
+
+                <div class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon "></span>
+                </div>
+                
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="additional.php">Home</a>
+                        </li>
+        
+                        <li class="nav-item">
+                            <a class="nav-link" href="additional.php">Institute</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="additional.php">Administration</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="additional.php">Library</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="additional.php">Department</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="additional.php">Admissions</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="additional.php">Prospectus</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="additional.php">E-Learning</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="additional.php">Contact Us</a>
+                        </li>
+        
+        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- navbar end -->
+        
+        <div class="main-bg"></div>
+        <div class="news-box">
+            
+            <div class="news mx-auto">
+                <div class="college-announcements">
+                    <p>College Announcements</p>
+                </div>
+                <div class="news-links">
+                    <div class="marquee">
+                        <a href="additional.php">NSS registration is open for all streams and year (2023-24). The last
+                            date for registration is 25/10/2023.<span class="new">New</span></a>
+                        <a href="additional.php">Application Form for Walk-in Interview for temporary faculty in Self
+                            –Financed Courses</a>
+                        <a href="additional.php">PGGC11: Notice for students<span class="new">New</span></a>
+                        <a href="additional.php">PGGC11 3rd Counselling for Boys Hostel Admission Session 2023-24</a>
+                        <a href="additional.php">Application Form for Walk-in Interview for temporary faculty in Self
+                            –Financed Courses</a>
+                        <a href="additional.php">PGGC11: Notice for students</a>
+                        <a href="additional.php">PGGC11 3rd Counselling for Boys Hostel Admission Session 2023-24<span
+                                class="new">New</span></a>
+                        <a href="additional.php">NSS registration is open for all streams and year (2023-24). The last
+                            date for registration is 25/10/2023.<span class="new">New</span></a>
+                        <a href="additional.php">Application Form for Walk-in Interview for temporary faculty in Self
+                            –Financed Courses</a>
+                        <a href="additional.php">PGGC11: Notice for students<span class="new">New</span></a>
+                        <a href="additional.php">PGGC11 3rd Counselling for Boys Hostel Admission Session 2023-24</a>
+                        <a href="additional.php">Application Form for Walk-in Interview for temporary faculty in Self
+                            –Financed Courses</a>
+                        </marquee>
+                    </div>
+                </div>
+            </div>
+
+            <div class="principal">
+
+                <h2>Principal's Message</h2>
+
+
+                <div class="row border-bottom">
+                    <div class="col-md-9 col-sm-8 col-12 principal-message">
+                        <p>Post Graduate Govt. College Sector 11, now a Co-educational Institute, was established in
+                            1953. Over the years, our illustrious students have carved a niche for themselves in fields
+                            as diverse as Politics, Administration, Business, Law etc. We offer a broad curriculum and a
+                            range of opportunities for all our students to achieve excellence in academic, creative,
+                            social, cultural, sporting and community endeavours.
+                            There are as many as 30 departments in Humanities, Commerce, Science, Information
+                            Technology, Physical Education as well as PG courses in Chemistry, Commerce, History, Hindi,
+                            English, Physics, Punjabi, Physical Education and Information Technology.
+                            The college boasts of a large sports ground with cinder track and a sports pavilion. There
+                            is also a swimming pool, a gymnasium, tennis, golf course and a basketball court. It has a
+                            department of Environment Education with a highly specialized Cosmic Garden, the first of
+                            its kind in India with special reference to Phytochemicals as an alternative medicine.
+                            Our college is spread over an area of 43.8 acres and is equipped with the latest technology,
+                            numerous internet labs, Wi-Fi facility, overall e-surveillance and the largest library among
+                            the college in this city. The library is fully computerized with online access and a new
+                            feature of transaction messaging through SMS. It has made old question papers and data of
+                            pending degrees since 1958 available online. It has a separate Braille corner for visually
+                            challenged students.
+                            Students, we believe are our most valued assets. A dedicated team of our teachers monitor
+                            and mentor young students to help them imbibe attributes, skills and capabilities to achieve
+                            their cherished goals. I am positive that in times to come, we will make steady progress in
+                            this rewarding journey of teaching and learning in Post Graduate Govt. College Sector 11,
+                            Chandigarh.</p>
+                    </div>
+                    <div class="col-md-3 col-sm-4 col-12 d-grid align-items-center principal-photo">
+                        <div>
+                            <div class="read-more">
+                                <a href="additional.php"><img src="img/principal.jpeg" alt="Principal Photo" id="principal"></a>
+                            </div>
+                            <h3>Prof (Dr.) Punam Agarwal</h2>
+                                <p>Principal</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="slidebox border">
+                <div class="slide-head">College Latest Events</div>
+                <div class="carousel-container">
+                    <div class="carousel-slide">
+                        <img src="img/slide1.jpeg" alt="Image 1">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide2.jpeg" alt="Image 2">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide3.jpeg" alt="Image 3">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide4.jpeg" alt="Image 4">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide5.jpeg" alt="Image 5">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide6.jpg" alt="Image 6">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide7.jpeg" alt="Image 7">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide8.jpeg" alt="Image 8">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide9.jpeg" alt="Image 9">
+                    </div>
+                    <div class="carousel-slide">
+                        <img src="img/slide10.jpeg" alt="Image 10">
+                    </div>
+                    <button class="prev-button">&#10094;</button>
+                    <button class="next-button">&#10095;</button>
+                </div>
+            </div>
+
+        </div>
+        <h2 class="gallery"></h2>
+        <!-- dream search start -->
+        <div class="w-100" id="dream-search">
+            <div class="container">
+                <div class="row py-5">
+
+                    <div class="col-lg-4 col-12">
+                        <div class="h3 text-light"> Find Course Now! </div>
+                    </div>
+
+                    <div class="col-md-auto col-12 py-lg-0 py-3">
+                        
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Qualification</option>
+                            <option value="">Undergraduate</option>
+                            <option value="">Postgraduate</option>
+                            <option value="">PhD</option>
+                            <option value="">Add on</option>
+                            <option value="">Diploma</option>
+                          </select>
+                    </div>
+                    <div class="col-md-auto col-12 py-lg-0 py-3">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Streams</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </select>
+                    </div>
+                    <div class="col-md-auto col-12 py-lg-0 py-3">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Courses</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </select>
+                    </div>
+                    <div class="col-md-auto col-12 py-lg-0 py-3">
+                        <button class="btn btn-dark"> Search Dream </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- dream serach end -->
+        
+        <div class="mt-5">
+
+            <h2 class="text-center">Our Gallery</h2>
+            <div class="cards">
+                <div class="card card1">
+                    <div class="content">
+                        <h1>Facility 1</h1>
+                        <p>Nature is beautiful</p>
+                    </div>
+                </div>
+                <div class="card card2">
+                    <div class="content">
+                        <h1>Facility 2</h1>
+                        <p>Nature is beautiful</p>
+                    </div>
+                </div>
+                <div class="card card3">
+                    <div class="content">
+                        <h1>Facility 3</h1>
+                        <p>Nature is beautiful</p>
+                    </div>
+                </div>
+                <div class="card card4">
+                    <div class="content">
+                        <h1>Facility 4</h1>
+                        <p>Nature is beautiful</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- footer -->
+        <div class="footer bg-danger">
+            <div class="ft-box">
+                <div class="h4 pt-4">External Links</div>
+                <a href="additional.php">Chandigarh Administration</a>
+                <a href="additional.php">University Grants Commission</a>
+                <a href="additional.php">Panjab University Chandigarh</a>
+                <a href="additional.php">Ministry of Human Resource Development</a>
+                <a href="additional.php">Rashtriya Uchchatar Shiksha Abhiyan</a>
+                <a href="additional.php">National Council for Teacher Education</a>
+            </div>
+            <div class="ft-box">
+                <div class="h4 pt-4">Infrastructure Facilities</div>
+                <a href="additional.php">Campus Infrastructure</a>
+                <a href="additional.php">Campus Facilities</a>
+                <a href="additional.php">Sports Facilities</a>
+                <a href="additional.php">Hostel Facilities</a>
+                <a href="additional.php">Physically Challenged Person Facilities</a>
+                <a href="additional.php">Affidavit for seeking recognition of M.Ped</a>
+            </div>
+            <div class="ft-box">
+                <div class="h4 pt-4">Other Links</div>
+                <a href="additional.php">Achievements</a>
+                <a href="additional.php">Gallery</a>
+                <a href="additional.php">Downloads</a>
+                <a href="additional.php">RTI</a>
+                <a href="additional.php">Cyber Crime Awareness</a>
+            </div>
+            <div class="ft-box">
+                <div class="h4 pt-4">Get In Touch</div>
+                <p>Post Graduate Government College,<br>
+                    Sector - 11, Madhya Marg,<br>
+                    Chandigarh, 160011<br>
+                    Phone No:+91-172–2740597<br>
+                    EMAIL ID : principal@gc11.ac.in<br>
+                    WEBSITE : www.gc11.ac.in</p>
+                <div class="social-icons">
+                    <a href="additional.php"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="additional.php"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="additional.php"><i class="fa-brands fa-square-instagram"></i></a>
+                    <a href="additional.php"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="copyright">
+            Copyright © Post Graduate Government College, Sector 11, Chandigarh, India |
+            
+            Designed by <a class="text-decoration-none text-light" href="https://github.com/VinaySharma77">Vinay Sharma</a>  &
+            
+            <a class="text-decoration-none text-light" href="https://github.com/MayankDevil/">Mayank</a>
+        
+        </div>
+    
+    </div>
+    <!-- bootstrap js -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <!-- js -->
+    <script src="js/zoom-in.js"></script>
+    <script src="js/normal.js"></script>
+    <script src="js/zoom-out.js"></script>
+    <script src="js/pop-up.js"></script>
+    <script src="js/carousel.js"></script>
+    <script src="js/menu.js"></script>
+</body>
+
+</html>
